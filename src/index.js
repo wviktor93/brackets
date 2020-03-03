67318d37342083c1a040aba3a1dfbd26afc89079
Victoria Triputina <triputina.victoria@gmail.com>
2020-03-03 15:14:52 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) {
+      return false;
+    }
+
+    bracketsConfig = bracketsConfig.map(item =>
+      item.join("")
+    )
+
+    let count = str.length;
+
+    for (let j = 0; j < count; j++) {
+      for (let i = 0; i < bracketsConfig.length; i++) {
+        if (str.indexOf(bracketsConfig[i]) >= 0) {
+          str = str.replace(bracketsConfig[i], "");
+        }
+      }
+    }
+
+    return str.length === 0 ? true : false;
+
+  }
